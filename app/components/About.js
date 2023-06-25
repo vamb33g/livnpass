3026f3eb0a0a60c341e2b276570f5ad69de2728e
vamb33g <fabs.webdev@gmail.com>
2023-06-25 21:27:40 +0200

    a

--- app/components/About.js
+++ app/components/About.js
@@ -9,8 +9,8 @@ const Analytics = () => {
       <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
       <div className="max-w-screen mx-auto grid md:grid-cols-2 pt-8">
         
-          <div className="flex flex-col justify-center">
-            <div className="p-8 bg-slate-400 text-center shadow-md rounded-lg pb-4">
+          <div className="flex flex-col justify-center pb-4">
+            <div className="p-7 bg-slate-400 text-center shadow-md rounded-lg">
               <p className="text-lg mb-2">
                 Fondée en 2016, notre entreprise offre deux services principaux :
               </p>
@@ -28,14 +28,15 @@ const Analytics = () => {
                 aux marchandises, et un service clientèle réactif.
               </p>
             </div>
-            <button className=" bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto lg:md:mx-0 py-3 hover:bg-gradient-to-l lg:md:w-[200px] lg:md:h-[60px] lg:md:text-xl">
+            
+          </div>
+          <button className=" bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto lg:md:mx-0 py-3 hover:bg-gradient-to-l lg:md:w-[200px] lg:md:h-[60px] lg:md:text-xl">
               <Link href={'/pages/about'}>En savoir plus</Link>
             </button>
-          </div>
         
        
           <img
-            src="/assets/about.png"
+            src="/assets/about2.png"
             alt="Laptop"
             className="w-full h-full object-cover"
           />
