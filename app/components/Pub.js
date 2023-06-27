b5832a27a6e945c1749b57bcd43af7e179c74659
vamb33g <fabs.webdev@gmail.com>
2023-06-27 06:08:21 +0200

    a

--- app/components/Pub.js
+++ app/components/Pub.js
@@ -4,33 +4,37 @@ import Image from 'next/image';
 import pub from '/public/assets/livnpass.png';
 import pub2 from '/public/assets/livnpass2.png';
 
-
 const Pub = () => {
-  
   return (
-    
-    <div className="flex flex-col md:flex-row lg:w-1/2">
-      <Image
-        alt="pub"
-        src={pub2}
-        sizes="100vw"
-        style={{
-          width: 'auto',
-          height: 'auto',
-        }}
-        className='animate-fade-right '
-      />
-      
-      <Image
-        alt="pub"
-        src={pub}
-        sizes="100vw"
-        style={{
-          width: 'auto',
-          height: 'auto',
-        }}
-       className="animate-fade-left md:w-1/2"
-      />
+    <div className="flex flex-col md:flex-row w-screen">
+     
+        <Image
+          alt="pub"
+          src="/assets/livnpass2.png"
+          width={500}
+          height={500}
+          className="animate-fade-left w-full md:w-1/2"
+        />
+        <Image
+          alt="pub"
+          src="/assets/livnpass.png"
+          width={500}
+          height={500}
+          className="animate-fade-left w-full md:w-1/2"
+        />
+     
+      {/* <div className="w-full">
+        <Image
+          alt="pub"
+          src={pub}
+          sizes="100vw"
+          style={{
+            width: 'auto',
+            height: 'auto',
+          }}
+          className="animate-fade-left"
+        />
+      </div> */}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,24 +28,8 @@ module.exports = {
         ping: {
           '75%, 100%': { transform: 'scale(1.1)', opacity: '0' },
         },
-        AnimationName: {
-          '0%, 100%': {
-            'background-position': '0% 50%',
-          },
-          '50%': {
-            'background-position': '100% 50%',
-          },
-        },
-        wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
-        },
+        
+        
         
       }
     },
